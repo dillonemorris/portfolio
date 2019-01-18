9202cf3b9723d4111a46aca1a05e0f90d504b352
Dillon Morris <dmorris@elmstreettechnology.com>
2019-01-18 14:31:00 -0700

    added custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,55 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import SEO from '../components/seo'
 import Container from '../components/Container'
+import Button from '../components/Button'
+import Error from '../images/error'
+import injectSheet from 'react-jss'
 
-const NotFoundPage = () => (
-  <>
+const NotFoundPage = ({ classes }) => (
+  <Fragment>
     <SEO title="404 - Not Found" />
     <Container>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className={classes.container}>
+        <Error />
+        <h1 className={classes.myHeading}>Woah!</h1>
+        <p className={classes.body}>
+          Looks like you're doing a bit of off-roading there...
+        </p>
+        <Button
+          color={'rgba(218, 162, 176,1)'}
+          border={'3px solid rgba(218, 162, 176,1)'}
+          boxShadow={'5px 5px rgba(218, 162, 176, 0.4)'}
+          boxShadowHover={'5px 5px rgba(218, 162, 176, 1)'}
+          text={'go back'}
+          fontSize={14}
+          page={'/projects/'}
+          padding={'6px 20px'}
+        />
+      </div>
     </Container>
-  </>
+  </Fragment>
 )
 
-export default NotFoundPage
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '40px auto 20px',
+    fontFamily:
+      "Inter UI, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif !important",
+  },
+  myHeading: {
+    fontSize: '48px',
+    paddingTop: '32px',
+    color: '#414141',
+    marginBottom: '12px',
+  },
+  body: {
+    color: '#606571',
+    fontSize: '20px',
+  },
+}
+
+export default injectSheet(styles)(NotFoundPage)
