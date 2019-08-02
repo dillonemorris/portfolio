9f01bf97c6e2cd0b9f274275f72da7e72bd02aed
Dillon Morris <dmorris@elmstreettechnology.com>
2019-08-01 17:21:23 -0700

    Updates to skills

--- src/data/skillData.js
+++ src/data/skillData.js
@@ -2,22 +2,17 @@ const skillData = {
   General: {
     title: 'General',
     skills: [
-      'Visual Design',
-      'UX Design',
-      'Responsive Design',
+      'Creative Problem Solving',
+      'Communication',
+      'UI/UX Design',
       'Design Principles',
       'Web Applications',
-      'Branding',
       'Front-End',
     ],
   },
-  Design: {
-    title: 'Design',
-    skills: ['Sketch', 'Invision', 'Rapid Prototyping'],
-  },
   Engineering: {
     title: 'Engineering',
-    skills: ['React.js', 'HTML5', 'CSS3', 'CSS in JS', 'Javascript'],
+    skills: ['React.js', 'CSS3', 'CSS in JS', 'Javascript', 'Gatsby', 'node'],
   },
 }
 
